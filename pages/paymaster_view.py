72f8dbb3065a9e8e01e63b9fd0d4850bfba3411d
Krzysztof Cislo <krzysztof.cislo265@gmail.com>
2026-02-15 19:23:05 +0100

    add possibility to mark players paid in paymaster view

--- pages/paymaster_view.py
+++ pages/paymaster_view.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from menu import menu_with_redirect
 from utils.db.games import get_games_repo, get_player_games, get_player_games_cost
-from utils.db.payments import get_payments_repo, get_player_payments_sum
+from utils.db.payments import Payment, get_payments_repo, get_player_payments_sum
 from utils.db.players import get_players_repo
 from utils.db.users import UserRole
 from utils.fb.api import Api
@@ -37,30 +37,28 @@ toast_notifications = ToastNotifications()
 
 
 with summary_tab:
-    with st.expander("Zawodnicy - podsumowanie płatności", expanded=True):
-        st.button("Odśwież")
-        all_games_cost = sum([g.cost * g.players_count for g in games])
-        all_players_payment = sum([pay.value for pay in payments])
-        cols = st.columns(3)
-        cols[0].write(f"Całkowity koszt: {all_games_cost} zł")
-        cols[1].write(f"Razem wpłat: {all_players_payment} zł")
-        all_balance = all_players_payment - all_games_cost
-        color_balance = "red" if all_balance < 0 else "green"
-        cols[2].write(f"Bilans: :{color_balance}[{all_balance} zł]")
-        players_to_show = [
-            {
-                "Zawodnik": p.fullname,
-                "Liczba gierek": len(get_player_games(games, p)),
-                "Zapłacono?": (pay_sum := get_player_payments_sum(payments, p))
-                >= (game_cost := get_player_games_cost(games, p)),
-                "Suma wpłat": pay_sum,
-                "Koszt gierek": game_cost,
-                "Bilans": (balance := pay_sum - game_cost),
-                "Gry opłacone z góry": balance // avg_game_cost,
-            }
-            for p in players
-        ]
-        st.dataframe(sorted(players_to_show, key=lambda x: x["Bilans"]))
+    all_games_cost = sum([g.cost * g.players_count for g in games])
+    all_players_payment = sum([pay.value for pay in payments])
+    cols = st.columns(3)
+    cols[0].write(f"Całkowity koszt: {all_games_cost} zł")
+    cols[1].write(f"Razem wpłat: {all_players_payment} zł")
+    all_balance = all_players_payment - all_games_cost
+    color_balance = "red" if all_balance < 0 else "green"
+    cols[2].write(f"Bilans: :{color_balance}[{all_balance} zł]")
+    players_to_show = [
+        {
+            "Zawodnik": p.fullname,
+            "Liczba gierek": len(get_player_games(games, p)),
+            "Zapłacono?": (pay_sum := get_player_payments_sum(payments, p))
+            >= (game_cost := get_player_games_cost(games, p)),
+            "Suma wpłat": pay_sum,
+            "Koszt gierek": game_cost,
+            "Bilans": (balance := pay_sum - game_cost),
+            "Gry opłacone z góry": balance // avg_game_cost,
+        }
+        for p in players
+    ]
+    st.dataframe(sorted(players_to_show, key=lambda x: x["Bilans"]))
 
 
 with notify_tab:
@@ -68,31 +66,44 @@ with notify_tab:
     players_to_show = [
         {
             "Zawodnik": pi.player.fullname,
-            "Bilans": pi.balance,
+            "Bilans": f"{pi.balance} zł",
         }
         for pi in late_players_infos
     ]
-    st.dataframe(players_to_show)
-
-    submit = st.button("Przypomnij wszystkim")
-    if submit:
-        for pi in late_players_infos:
-            try:
-                notification = send_cash_notification(pi.player, abs(pi.balance))
-            except Exception as e:
-                toast_notifications.add(msg=str(e), icon="❌")
-            else:
-                toast_notifications.add(
-                    msg=f"Powiadomienie do '{pi.player.fullname}' zostało wysłane!", icon="✅"
-                )
-        st.rerun()
+    if players_to_show:
+        st.dataframe(players_to_show)
+        cols = st.columns(2)
+        remind_all = cols[0].button("Przypomnij wszystkim")
+        mark_all_paid = cols[1].button("Zaznacz, że wszyscy zapłacili")
+        if remind_all:
+            for pi in late_players_infos:
+                try:
+                    notification = send_cash_notification(pi.player, abs(pi.balance))
+                except Exception as e:
+                    toast_notifications.add(msg=str(e), icon="❌")
+                else:
+                    toast_notifications.add(
+                        msg=f"Powiadomienie do '{pi.player.fullname}' zostało wysłane!", icon="✅"
+                    )
+            st.rerun()
+        if mark_all_paid:
+            for pi in late_players_infos:
+                try:
+                    payments_repo.save(Payment(player_id=str(pi.player.id), value=abs(pi.balance)))
+                except Exception as e:
+                    toast_notifications.add(msg=str(e), icon="❌")
+                else:
+                    toast_notifications.add(
+                        msg=f"Zawodnik '{pi.player.fullname}' zapłacił!", icon="✅"
+                    )
+            st.rerun()
 
 
 with rest_tab:
     with st.container(border=True):
         st.warning("Ta funkcjonalność jest eksperymentalna")
-        submit = st.button("Pobierz wszystkich korespondentów")
-        if submit:
+        remind_all = st.button("Pobierz wszystkich korespondentów")
+        if remind_all:
             api = Api()
             ps = api.get_all_unique_participants()
             st.dataframe(ps)
@@ -107,11 +118,11 @@ with rest_tab:
             options=players,
             format_func=lambda p: p.fullname,
         )
-        submit = False
+        remind_all = False
         if player_to_notify:
             amount = st.number_input("Kwota do zapłaty (zł)", key="notify_amount", min_value=0)
-            submit = st.button("Wyślij powiadomienie")
-            if submit:
+            remind_all = st.button("Wyślij powiadomienie")
+            if remind_all:
                 if not amount:
                     st.error("Kwota jest wymagana!")
                 else:
